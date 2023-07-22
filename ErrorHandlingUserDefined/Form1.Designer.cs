db700eda54a9f4528b283f0db891b869594c0623
Jerome Marco <jerome.marco.pixel8@gmail.com>
2023-07-22 11:21:20 +0800

    first commit

---
+++ ErrorHandlingUserDefined/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿
+namespace ErrorHandlingUserDefined
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEvaluate = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtInputName = new System.Windows.Forms.TextBox();
+            this.txtCorrectName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnEvaluate
+            // 
+            this.btnEvaluate.Location = new System.Drawing.Point(387, 283);
+            this.btnEvaluate.Name = "btnEvaluate";
+            this.btnEvaluate.Size = new System.Drawing.Size(75, 23);
+            this.btnEvaluate.TabIndex = 14;
+            this.btnEvaluate.Text = "Evaluate";
+            this.btnEvaluate.UseVisualStyleBackColor = true;
+            this.btnEvaluate.Click += new System.EventHandler(this.btnEvaluate_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(299, 188);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Input Name:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(299, 144);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Correct Name:";
+            // 
+            // txtInputName
+            // 
+            this.txtInputName.Location = new System.Drawing.Point(402, 185);
+            this.txtInputName.Name = "txtInputName";
+            this.txtInputName.Size = new System.Drawing.Size(100, 20);
+            this.txtInputName.TabIndex = 11;
+            // 
+            // txtCorrectName
+            // 
+            this.txtCorrectName.Location = new System.Drawing.Point(402, 144);
+            this.txtCorrectName.Name = "txtCorrectName";
+            this.txtCorrectName.Size = new System.Drawing.Size(100, 20);
+            this.txtCorrectName.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnEvaluate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtInputName);
+            this.Controls.Add(this.txtCorrectName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEvaluate;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtInputName;
+        private System.Windows.Forms.TextBox txtCorrectName;
+    }
+}
+
---
+++ ErrorHandlingUserDefined/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ErrorHandlingUserDefined
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEvaluate_Click(object sender, EventArgs e)
+        {
+            Names NameVar = new Names();
+
+            try
+            {
+                NameVar.showName(txtCorrectName.Text, txtInputName.Text);
+            }
+            catch (InvalidNameException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                MessageBox.Show("Thanks");
+            }
+        }
+    }
+    public class InvalidNameException : ApplicationException
+    {
+        public InvalidNameException(string message)
+            : base(message)
+        { }
+
+    }
+    class Names
+    {
+        public void showName(string correctName, string inputName)
+        {
+            if (correctName != inputName)
+            {
+                throw (new InvalidNameException("Invalid Name found"));
+            }
+            else
+            {
+                MessageBox.Show("Correct Name is: " + correctName);
+            }
+        }
+
+    }
+}
